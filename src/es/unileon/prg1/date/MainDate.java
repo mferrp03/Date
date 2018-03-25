7f499e8a645416fa658377e19c37a62ce36571c2
mferrp03 <mferrp03@estudiantes.unileon.es>
2018-03-25 18:20:56 +0200

    a ver si es verdad

---
+++ src/es/unileon/prg1/date/MainDate.java
@@ -0,0 +1,15 @@
+package es.unileon.prg1.date;
+
+public class MainDate {
+
+	public static void main(String[] args) {
+		Date today, tomorrow;
+		today = new Date(20, 3, 2018);
+		tomorrow = new Date(20, 3, 2018);
+		System.out.println(today + " isSameYear " + tomorrow + "? " + today.isSameYear(tomorrow));
+		System.out.println(today + " isSameMonth " + tomorrow + "? " + today.isSameMonth(tomorrow));
+		System.out.println(today + " isSameDay " + tomorrow + "? " + today.isSameDay(tomorrow));
+		System.out.println(today + " isSame " + tomorrow + "? " + today.isSame(tomorrow));
+	}
+
+}
