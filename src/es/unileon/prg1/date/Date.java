b78315586a9d2e5e5c16e6d9f396d3ad81c4a2f3
mferrp03 <mferrp03@estudiantes.unileon.es>
2018-03-27 17:22:44 +0200

    yo ya no se que hacer

--- src/es/unileon/prg1/date/Date.java
+++ src/es/unileon/prg1/date/Date.java
@@ -46,6 +46,139 @@ public class Date {
 			}
 		return false;
 	}
+	public String getMonthName(){
+		String name = new String();
+		switch(this.month){
+			case 1:
+			name = "January";
+			break;
+			case 2:
+			name = "February";
+			break;
+			case 3:
+			name = "March";
+			break;
+			case 4:
+			name = "April";
+			break;
+			case 5:
+			name = "May";
+			break;
+			case 6:
+			name = "June";
+			break;
+			case 7:
+			name = "July";
+			break;
+			case 8:
+			name = "August";
+			break;
+			case 9:
+			name = "September";
+			break;
+			case 10:
+			name = "October";
+			break;
+			case 11:
+			name = "November";
+			break;
+			case 12:
+			name = "December";
+			break;
+			}
+		return name;
+	}
+	public boolean isDayOfMonthRight(){
+		boolean correcto = false;
+		switch(this.month){
+			case 1:
+				if(this.day<=31)
+					correcto = true;
+				break;
+			case 3:
+				if(this.day<=31)
+					correcto = true;
+				break;
+			case 5:
+				if(this.day<=31)
+					correcto = true;
+				break;
+			case 7:
+				if(this.day<=31)
+					correcto = true;
+				break;
+			case 8:
+				if(this.day<=31)
+					correcto = true;
+				break;
+			case 10:
+				if(this.day<=31)
+					correcto = true;
+				break;
+			case 12:
+				if(this.day<=31)
+					correcto = true;
+				break;
+			case 2:
+				if(this.day<=28)
+					correcto = true;
+				break;
+			case 4:
+				if(this.day<=30)
+					correcto = true;
+				break;
+			case 6:
+				if(this.day<=30)
+					correcto = true;
+				break;
+			case 9:
+				if(this.day<=30)
+					correcto = true;
+				break;
+			case 11:
+				if(this.day<=30)
+					correcto = true;
+				break;
+		}
+		return correcto;
+	}
+	public String seasonOfTheMonth(){
+		String season = new String();
+		switch(this.month){
+			case 1:
+			case 2:
+			case 3:
+				season = "winter";
+				break;
+			case 4:
+			case 5:
+			case 6:
+				season = "spring";
+				break;
+			case 7:
+			case 8:
+			case 9:
+				season = "summer";
+				break;
+			case 10:
+			case 11:
+			case 12:
+				season = "fall";
+				break;
+			}
+		return season;
+	}
+	public String monthsLeft(){
+
+ 	 StringBuilder months = new StringBuilder();
+
+ 	for(int i=this.month+1;i<=12;i++){
+
+    		months.append(this.getMonthName()); }
+
+ 	return months.toString();
+
+}
 	public String toString(){
 		return this.day + "/" + this.month + "/" + this.year;
 	}
